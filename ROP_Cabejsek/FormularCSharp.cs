7989c2c781600a26c8af72828e509fc701b4727a
Cabejšek Tomáš <sarryxgg@gmail.com>
2024-01-11 18:44:21 +0100

    c# txt

--- ROP_Cabejsek/FormularCSharp.cs
+++ ROP_Cabejsek/FormularCSharp.cs
@@ -20,84 +20,7 @@ namespace ROP_Cabejsek
         Priklady formPriklady = new Priklady();
         string[] separators = { "\r\n", "\r", "\n" };
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            for (int i = formPriklady.Controls.Count - 1; i >= 0; i--)
-            {
-                Control control = formPriklady.Controls[i];
-
-                // Pokud je ovládací prvek TextBox nebo Label, odstraň jej
-                if (control is TextBox || control is Label)
-                {
-                    formPriklady.Controls.Remove(control);
-                }
-            }
-
-            // Aktualizuje a překresluje formulář
-            formPriklady.Refresh();
-            using (StreamReader sr = new StreamReader(@"..\..\..\Soubory\CSharp\PrikladyRidStruk.txt"))
-            {
-                string s;
-                string[] sts;
-                if (!sr.EndOfStream)
-                {
-                    s = sr.ReadToEnd();
-                    sts = s.Split(separators, StringSplitOptions.None);
-
-                    int verticalSpacing = 30;
-                    int currentTop = 10;
-
-                    //pridani teorie na zacatek label
-                    Label labelTeorie = new Label();
-                    labelTeorie.Text = "Teorie";
-                    labelTeorie.AutoSize = true;
-                    labelTeorie.Top = currentTop;
-                    formPriklady.Controls.Add(labelTeorie);
-                    currentTop += verticalSpacing;
-                    //pridani tb s teorii
-                    TextBox tba = new TextBox();
-                    tba.Multiline = true;
-                    tba.ScrollBars = ScrollBars.Vertical;
-                    tba.Text = sts[0];
-                    tba.Width = formPriklady.ClientSize.Width - 20;
-                    tba.Height = 60;
-                    tba.Top = currentTop;
-                    tba.ReadOnly = true;
-                    formPriklady.Controls.Add(tba);
-                    currentTop += tba.Height + verticalSpacing;
-
-
-                    for (int i = 1; i < sts.Length; i++)
-                    {
-                        Label labelNazev = new Label();
-                        labelNazev.Text = "Příklad č." + (i);
-                        labelNazev.AutoSize = true;
-                        labelNazev.Top = currentTop;
-
-                        formPriklady.Controls.Add(labelNazev);
-                        currentTop += verticalSpacing;
-
-
-                        TextBox tb = new TextBox();
-                        tb.Multiline = true; // Použití víceřádkového textového pole
-                        tb.ScrollBars = ScrollBars.Vertical; // Přidání svislého skrolování, pokud text přesahuje
-                        tb.Text = sts[i];
-
-                        // Nastavení šířky textového pole na šířku formuláře s odčtením mezery (např. 20)
-                        tb.Width = formPriklady.ClientSize.Width - 20;
-
-                        tb.Height = 60; // Nastavení výšky textového pole
-                        tb.Top = currentTop;
-                        tb.ReadOnly = true;
-
-                        formPriklady.Controls.Add(tb);
-                        currentTop += tb.Height + verticalSpacing;
-                    }
-                }
-
-                formPriklady.ShowDialog();
-            }
-        }
+        
 
         private void FormularCSharp_FormClosed(object sender, FormClosedEventArgs e)
         {
@@ -186,6 +109,86 @@ namespace ROP_Cabejsek
                 formPriklady.ShowDialog();
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = formPriklady.Controls.Count - 1; i >= 0; i--)
+            {
+                Control control = formPriklady.Controls[i];
+
+                // Pokud je ovládací prvek TextBox nebo Label, odstraň jej
+                if (control is TextBox || control is Label)
+                {
+                    formPriklady.Controls.Remove(control);
+                }
+            }
+
+            // Aktualizuje a překresluje formulář
+            formPriklady.Refresh();
+            using (StreamReader sr = new StreamReader(@"..\..\..\Soubory\CSharp\PrikladyRidStruk.txt"))
+            {
+                string s;
+                string[] sts;
+                if (!sr.EndOfStream)
+                {
+                    s = sr.ReadToEnd();
+                    sts = s.Split(separators, StringSplitOptions.None);
+
+                    int verticalSpacing = 30;
+                    int currentTop = 10;
+
+                    //pridani teorie na zacatek label
+                    Label labelTeorie = new Label();
+                    labelTeorie.Text = "Teorie";
+                    labelTeorie.AutoSize = true;
+                    labelTeorie.Top = currentTop;
+                    formPriklady.Controls.Add(labelTeorie);
+                    currentTop += verticalSpacing;
+                    //pridani tb s teorii
+                    TextBox tba = new TextBox();
+                    tba.Multiline = true;
+                    tba.ScrollBars = ScrollBars.Vertical;
+                    tba.Text = sts[0];
+                    tba.Width = formPriklady.ClientSize.Width - 20;
+                    tba.Height = 60;
+                    tba.Top = currentTop;
+                    tba.ReadOnly = true;
+                    formPriklady.Controls.Add(tba);
+                    currentTop += tba.Height + verticalSpacing;
+
+
+                    for (int i = 1; i < sts.Length; i++)
+                    {
+                        Label labelNazev = new Label();
+                        labelNazev.Text = "Příklad č." + (i);
+                        labelNazev.AutoSize = true;
+                        labelNazev.Top = currentTop;
+
+                        formPriklady.Controls.Add(labelNazev);
+                        currentTop += verticalSpacing;
+
+
+                        TextBox tb = new TextBox();
+                        tb.Multiline = true; // Použití víceřádkového textového pole
+                        tb.ScrollBars = ScrollBars.Vertical; // Přidání svislého skrolování, pokud text přesahuje
+                        tb.Text = sts[i];
+
+                        // Nastavení šířky textového pole na šířku formuláře s odčtením mezery (např. 20)
+                        tb.Width = formPriklady.ClientSize.Width - 20;
+
+                        tb.Height = 60; // Nastavení výšky textového pole
+                        tb.Top = currentTop;
+                        tb.ReadOnly = true;
+
+                        formPriklady.Controls.Add(tb);
+                        currentTop += tb.Height + verticalSpacing;
+                    }
+                }
+
+                formPriklady.ShowDialog();
+            }
+        }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
